d6b8da03da8b6cd66570c7f8745b71859afc1242
Tithira De Silva <tithiradesilva@gmail.com>
2024-07-12 02:18:13 +0530

    Contact Us Done
    Need to setup the form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {ImageCarousel, NavigationBar, Whatwedo, Whoarewe } from './components';
+import './index.css';
+import ContactUs from './components/contactUs/contactUs';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
-      <ImageCarousel />
-      <Whoarewe />
-
-      {/* Carousel goes here */}
-    <div className="bg-white py-24 sm:py-32">
-      <Whatwedo />
-      
-      </div>
+    <div>
+      <ContactUs />
     </div>
   );
 }
 
 export default App;
-
