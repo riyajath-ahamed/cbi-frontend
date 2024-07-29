90a2203f2aeeabce73fc006f5f1843b40ae4654e
riyajath-ahamed <riyajatha@gmail.com>
2024-07-29 23:15:31 +0530

    scroll to top | RJ

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {  Blog, Gallery, ImageCarousel, MissionCard, OurImpact, Project, QuotesSlider, Whatwedo, WhereWeWork, Whoarewe } from '../index'
+import { ScrollToTopButton } from '../common'
 
 const Home = () => {
   return (
     <div className=''>
       <ImageCarousel  />
       <Whoarewe />
+      <ScrollToTopButton />
 
       {/* Carousel goes here */}
 
