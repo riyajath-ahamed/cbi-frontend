505fb936a85e94945daccc1d6fc3de275213f5d5
riyajath-ahamed <riyajatha@gmail.com>
2024-08-09 00:12:08 +0530

    Minor | RJ

--- src/components/common/blogWait.jsx
+++ src/components/common/blogWait.jsx
@@ -2,20 +2,20 @@
 const BlogWait = () => {
     return (
         <div className="mx-auto mt-12 max-w-2xl sm:mt-16 lg:mt-16 lg:max-w-6xl">
-            <div class="animate-pulse flex flex-col items-center gap-4 w-96">
+            <div className="animate-pulse flex flex-col items-center gap-4 w-96">
                 <div>
-                    <div class="w-48 h-6 bg-slate-400 rounded-md"></div>
-                    <div class="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
+                    <div className="w-48 h-6 bg-slate-400 rounded-md"></div>
+                    <div className="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
                 </div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-1/2 rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="h-7 bg-slate-400 w-full rounded-md"></div>
-                <div class="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
-                <div class="w-48 h-6 bg-slate-400 rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-1/2 rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="h-7 bg-slate-400 w-full rounded-md"></div>
+                <div className="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
+                <div className="w-48 h-6 bg-slate-400 rounded-md"></div>
             </div>
         </div>
     )
