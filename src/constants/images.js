6e135756883b3028154e3345792f59e68f6531d2
riyajath-ahamed <riyajatha@gmail.com>
2024-08-14 23:52:27 +0530

    Gallery | RJ

---
+++ src/constants/images.js
@@ -0,0 +1,29 @@
+export const imagesIds = [
+  {
+    id: "1zlFbTWrXfDKW3lcESVlv8DL3C8onaGdy",
+  },
+  {
+    id: "1ntUMyrkEhJxua1sck9I0oKIB6igQ3PjT",
+  },
+  {
+    id: "1JZwNVKDRn7IFH_6ho-qZjfBxh4OHjQH2",
+  },
+  {
+    id: "1TJjz99UVqlXGCe1bzToBukMw7TOJxD3F",
+  },
+  {
+    id: "1xME3tcXJCZKr-uFpU06gZA5FuZmRe88p",
+  },
+  {
+    id: "1OZUK9SQ7Axc5wDHkcGMGdLmU07Kei-pw",
+  },
+  {
+    id: "1_TA7Q-HZ6RD00IsJi-RyAOPEESnBZMJt",
+  },
+  {
+    id: "10QfMASOqzm0Cjpg8oT3qE4JrKN7tqMTd",
+  },
+  {
+    id: "1lYuw-qNyarz1-zDE3DNXBAHngBbca0E7",
+  },
+];
