c2426edfa11651c1b28bfbfab758a4e92bdf6316
Tithira De Silva <tithiradesilva@gmail.com>
2024-12-27 00:36:26 +0530

    Changes
    Changes

--- src/components/whereWeWork/whereWeWork.jsx
+++ src/components/whereWeWork/whereWeWork.jsx
@@ -1,50 +1,13 @@
-import React  from 'react';
-// import createGlobe from 'cobe';
+import React from 'react';
 
 const WhereWeWork = () => {
-  // const canvasRef = useRef();
-
-  // useEffect(() => {
-  //   let phi = 0;
-
-  //   const globe = createGlobe(canvasRef.current, {
-  //     devicePixelRatio: 2,
-  //     width: 600 * 2,
-  //     height: 600 * 2,
-  //     phi: 0,
-  //     theta: 0,
-  //     dark: 0,
-  //     diffuse: 1.2,
-  //     mapSamples: 16000,
-  //     mapBrightness: 6,
-  //     baseColor: [1, 1, 1],
-  //     markerColor: [0.133, 0.773, 0.369],
-  //     glowColor: [1, 1, 1],
-  //     markers: [
-  //       // longitude latitude
-  //       { location: [-37.8136, 144.9631], size: 0.05 }, // Melbourne
-  //       { location: [7.8731, 80.7718], size: 0.05 } // Sri Lanka
-  //       // Add more markers here
-  //     ],
-  //     onRender: (state) => {
-  //       // Called on every animation frame.
-  //       // `state` will be an empty object, return updated params.
-  //       state.phi = phi;
-  //       phi += 0.01;
-  //     }
-  //   });
-
-  //   return () => {
-  //     globe.destroy();
-  //   };
-  // }, []);
-
-  // TODO: Add more markers to the globe
-  //       Center the globe 
-
   return (
-    <div id='wherewework' className='max-w-[90rem] w-full mx-auto flex flex-1 items-center justify-center' style={{ height: '100vh' }}>
-      <div className='py-10 flex-col pb-8 w-full justify-center max-w-full flex min-w-0'>
+    <div
+      className="max-w-[90rem] w-full mx-auto flex flex-1 items-center justify-center"
+      style={{ height: '100vh' }}
+    >
+      <div className="py-10 flex-col pb-8 w-full justify-center max-w-full flex min-w-0">
+        {/* Heading Section */}
         <div className="mx-auto max-w-2xl lg:text-center">
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Where we work
@@ -54,15 +17,17 @@ const WhereWeWork = () => {
           </p>
         </div>
 
-        <div className='max-w-4xl px-6 md:px-8 pt-4 z-10 min-w-0 w-full flex justify-between'>
-          {/* <canvas
-            ref={canvasRef}
-            style={{ width: 600, height: 600, maxWidth: '100%', aspectRatio: 1 }}
-          /> */}
+        {/* Image Section */}
+        <div className="mt-8 flex justify-center">
+          <img 
+            src="world-map2.png" 
+            alt="Where we work illustration" 
+            className="max-w-full h-auto" 
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default WhereWeWork;
