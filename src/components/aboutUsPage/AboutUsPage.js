afd28d9ff35eed8218e624e8fb04c4053decfce5
Tithira De Silva <tithiradesilva@gmail.com>
2025-01-09 23:41:18 +0530

    update
    update

--- src/components/aboutUsPage/AboutUsPage.js
+++ src/components/aboutUsPage/AboutUsPage.js
@@ -4,109 +4,110 @@ import tithiraImage from "../../assets/image_tithira.png"; // Adjust the relativ
 
 
 const teamData = {
-  directors: [
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-  ],
-  managers: [
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-  ],
-  advisors: [
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    // Add the rest of the advisors...
-  ],
-  sriLankaProjectTeam: [
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-  ],
-  members: [
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
-    // Add the rest of the members...
-  ],
+    directors: [
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+    ],
+    managers: [
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+    ],
+    advisors: [
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        // Add the rest of the advisors...
+    ],
+    sriLankaProjectTeam: [
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+    ],
+    members: [
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        { name: "Tithira De Silva", position: "CEO", imageUrl: tithiraImage },
+        // Add the rest of the members...
+    ],
 };
 
 const AboutUsPage = () => {
-  // Define the renderTeam function
-  const renderTeam = (title, members) => {
-    return (
-        <div className="mb-16">
-          <h3 className="text-2xl font-medium text-center mb-20">{title}</h3>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 gap-y-20 mb-20 justify-center mx-auto place-items-center">
-            {members.map((member, index) => (
-              <div
-                key={index}
-                className="text-center flex flex-col items-center justify-center transition-transform transform hover:scale-125"
-              >
-                <img
-                  src={member.imageUrl}
-                  alt={member.name}
-                  className="w-36 h-36 rounded-full mb-4 border-2 border-gray-200"
-                />
-                <p className="font-medium text-lg">{member.name}</p>
-                <p className="text-sm text-gray-500">{member.position}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-    );                      
-};
+    // Define the renderTeam function
+    const renderTeam = (title, members) => {
+        return (
+            <div className="mb-16">
+                <h3 className="text-2xl font-medium text-center mb-20 mt-20">{title}</h3>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 gap-y-20 mt-30 justify-center">
+                    {members.map((member, index) => (
+                        <div
+                            key={index}
+                            className="text-center flex flex-col items-center justify-center transition-transform transform hover:scale-125"
+                        >
+                            <img
+                                src={member.imageUrl}
+                                alt={member.name}
+                                className="w-36 h-36 rounded-full mb-4 border-2 border-gray-200"
+                            />
+                            <p className="font-medium text-lg">{member.name}</p>
+                            <p className="text-sm text-gray-500">{member.position}</p>
+                        </div>
+                    ))}
+                </div>
 
-
-  return (
-    <div className="bg-white text-gray-900">
-      {/* About Us Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto lg:px-24 px-8">
-          <h1 className="text-4xl font-bold text-center mb-2">About Us</h1>
-          <p className="text-center text-gray-600 text-lg leading-loose">
-            Let's know who we are and what we are doing
-          </p>
-          <div className="mt-8 text-gray-700 mb-12">
-            <p className="text-lg mb-10 leading-loose">
-              We are an Australian <span className="underline underline-offset-8">registered</span> public guaranteed company working towards education, health, economic & skill development.
-            </p>
-            <div className="lg:flex xl:flex mt-4 mb-6">
-              <p className="text-lg leading-loose">
-                Beyond Childhood International is a <span className="underline underline-offset-8">non-partisan</span> development organisation dedicated to work with children, youth families, and communities to overcome challenges like reduced parental care, economic stress and vulnerabilities, social norms and attitudes, and skill gaps to meet the demands of the national and international labor market.
-                <br /><br />
-                We work through our main sectors like <span className="underline underline-offset-8">education, health, nutrition, economic development, and child protection</span> – serving all people, regardless of religion, race, ethnicity, or gender.
-              </p>
-              <img src="https://placehold.co/300x210" alt="A group of children in a classroom setting" className="rounded-lg lg:ml-10 mt-6 lg:mt-0 mx-auto transition-transform transform hover:scale-105" />
             </div>
-            <p className="text-lg leading-loose">
-              We empower vulnerable communities to transform their lives by promoting International Best Practices and also we aim to share international experiences and build regional and international partnerships to enable vulnerable communities to attain their full potential, regardless of ability or background. As an organization focused on strategic development actions, we provide a platform for vulnerable communities to meet international standards, avenues, and paths for personal, communal, and national development.
-            </p>
-          </div>
-        </div>
-      </section>
+        );
+    };
 
-      {/* Meet the Team Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-4">Meet the Team</h2>
-          <p className="text-center text-lg text-gray-600 leading-loose mb-12">
-            We've got an entire team dedicated to supporting you through the projects.
-          </p>
-          {renderTeam("Directors", teamData.directors)}
-          {renderTeam("Managers", teamData.managers)}
-          {renderTeam("Advisors", teamData.advisors)}
-          {renderTeam("Sri Lanka Project Team", teamData.sriLankaProjectTeam)}
-          {renderTeam("Members", teamData.members)}
+
+    return (
+        <div className="bg-white text-gray-900">
+            {/* About Us Section */}
+            <section className="py-12">
+                <div className="max-w-7xl mx-auto lg:px-24 px-8">
+                    <h1 className="text-4xl font-bold text-center mb-2">About Us</h1>
+                    <p className="text-center text-gray-600 text-lg leading-loose">
+                        Let's know who we are and what we are doing
+                    </p>
+                    <div className="mt-8 text-gray-700 mb-12">
+                        <p className="text-lg mb-10 leading-loose">
+                            We are an Australian <span className="underline underline-offset-8">registered</span> public guaranteed company working towards education, health, economic & skill development.
+                        </p>
+                        <div className="lg:flex xl:flex mt-4 mb-6">
+                            <p className="text-lg leading-loose">
+                                Beyond Childhood International is a <span className="underline underline-offset-8">non-partisan</span> development organisation dedicated to work with children, youth families, and communities to overcome challenges like reduced parental care, economic stress and vulnerabilities, social norms and attitudes, and skill gaps to meet the demands of the national and international labor market.
+                                <br /><br />
+                                We work through our main sectors like <span className="underline underline-offset-8">education, health, nutrition, economic development, and child protection</span> – serving all people, regardless of religion, race, ethnicity, or gender.
+                            </p>
+                            <img src="https://placehold.co/300x210" alt="A group of children in a classroom setting" className="rounded-lg lg:ml-10 mt-6 lg:mt-0 mx-auto transition-transform transform hover:scale-105" />
+                        </div>
+                        <p className="text-lg leading-loose">
+                            We empower vulnerable communities to transform their lives by promoting International Best Practices and also we aim to share international experiences and build regional and international partnerships to enable vulnerable communities to attain their full potential, regardless of ability or background. As an organization focused on strategic development actions, we provide a platform for vulnerable communities to meet international standards, avenues, and paths for personal, communal, and national development.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            {/* Meet the Team Section */}
+            <section className="py-12 mb-20">
+                <div className="max-w-7xl mx-auto px-4">
+                    <h2 className="text-3xl font-bold text-center mb-4">Meet the Team</h2>
+                    <p className="text-center text-lg text-gray-600 leading-loose mb-12">
+                        We've got an entire team dedicated to supporting you through the projects.
+                    </p>
+                    {renderTeam("Directors", teamData.directors)}
+                    {renderTeam("Managers", teamData.managers)}
+                    {renderTeam("Advisors", teamData.advisors)}
+                    {renderTeam("Sri Lanka Project Team", teamData.sriLankaProjectTeam)}
+                    {renderTeam("Members", teamData.members)}
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default AboutUsPage;
--- src/components/contactUs/contactUs.js
+++ src/components/contactUs/contactUs.js
@@ -12,7 +12,7 @@ const ContactUs = () => {
       <h4 className="text-center text-gray-600 mb-[100px] text-[18px]">Reach out to us for any inquiries, support, or feedback.</h4>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center mt-15 mb-[60px]">
         <div>
-            <FaLocationDot className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
+          <FaLocationDot className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
           <h2 className="text-xl font-medium mb-[9px] text-[25px]">Address</h2>
           <p className="font-semiboldfont-575757 text-[#363636] text-[19px]">Australia</p>
           <p className="font-medium text-[#8A8A8A] mb-[26px]  text-[16px]">Melbourne, Victoria, <br></br>Australia.</p>
@@ -20,15 +20,15 @@ const ContactUs = () => {
           <p className="font-medium text-[#8A8A8A]  text-[16px] mb-[20px]">Colombo, Western Province, <br></br>Sri Lanka.</p>
         </div>
         <div>
-        <FaPhoneAlt className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
-        <h2 className="text-xl font-medium mb-[9px] text-[25px]">Phone</h2>
-        <p className="font-semiboldfont-575757 text-[#363636] text-[19px]">Australia</p>
+          <FaPhoneAlt className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
+          <h2 className="text-xl font-medium mb-[9px] text-[25px]">Phone</h2>
+          <p className="font-semiboldfont-575757 text-[#363636] text-[19px]">Australia</p>
           <p className="font-roboto font-semibold text-[#8A8A8A] mb-[46px]  text-[15px]">+61 491 570 006</p>
           <p className="font-semiboldfont-575757 text-[#363636] text-[19px]">Sri Lanka</p>
           <p className="font-roboto font-semibold text-[#8A8A8A] mb-[26px]  text-[15px] font-Inter">+94 12 34 567 89</p>
         </div>
         <div>
-        <MdEmail className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
+          <MdEmail className=" mx-auto mb-4 bg-primary p-5 rounded-full text-white w-24 h-24 transform transition-transform duration-300 hover:scale-[1.1]" />
           <h2 className="text-xl font-medium mb-[9px] text-[25px]">Email</h2>
           <p className="font-semiboldfont-575757 text-[#363636] text-[19px]">For Donations</p>
           <p className="font-medium text-[#8A8A8A] mb-[46px]  text-[16px]">sampleemail@gmail.com</p>
@@ -42,18 +42,18 @@ const ContactUs = () => {
         <div className="bg-[#262626] text-center text-white py-7 mb-12 rounded-[8px] px-4 md:px-8 lg:px-16 mx-auto max-w-screen-xl">
           <h2 className="text-[24px] sm:text-[28px] md:text-[28px] lg:text-[32px] font-medium mb-[35px] mt-[35px]">Connect with Us on Social Media</h2>
           <div className="flex justify-center space-x-6 text-2xl sm:text-3xl md:text-[30px] lg:text-[30px] mb-14">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-facebook-f text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-instagram text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
-          </a>
-          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-youtube text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
-          </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
-          </a>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-f text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
+            </a>
+            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-youtube text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin text-[#2BC581] transform transition-transform duration-300 hover:scale-[1.3]"></i>
+            </a>
           </div>
         </div>
       </div>
