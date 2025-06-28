9a73f12ad0c6ae7c6bc323dcd21bf89794817ba3
riyajath-ahamed <riyajatha@gmail.com>
2025-06-28 23:48:39 +0530

    World Map | RJ

--- src/components/whereWeWork/whereWeWork.jsx
+++ src/components/whereWeWork/whereWeWork.jsx
@@ -1,46 +1,7 @@
 import React  from 'react';
-// import createGlobe from 'cobe';
+import MapChart from '../worldMap/worldMap';
 
 const WhereWeWork = () => {
-  // const canvasRef = useRef();
-
-  // useEffect(() => {
-  //   let phi = 0;
-
-  //   const globe = createGlobe(canvasRef.current, {
-  //     devicePixelRatio: 2,
-  //     width: 600 * 2,
-  //     height: 600 * 2,
-  //     phi: 0,
-  //     theta: 0,
-  //     dark: 0,
-  //     diffuse: 1.2,
-  //     mapSamples: 16000,
-  //     mapBrightness: 6,
-  //     baseColor: [1, 1, 1],
-  //     markerColor: [0.133, 0.773, 0.369],
-  //     glowColor: [1, 1, 1],
-  //     markers: [
-  //       // longitude latitude
-  //       { location: [-37.8136, 144.9631], size: 0.05 }, // Melbourne
-  //       { location: [7.8731, 80.7718], size: 0.05 } // Sri Lanka
-  //       // Add more markers here
-  //     ],
-  //     onRender: (state) => {
-  //       // Called on every animation frame.
-  //       // `state` will be an empty object, return updated params.
-  //       state.phi = phi;
-  //       phi += 0.01;
-  //     }
-  //   });
-
-  //   return () => {
-  //     globe.destroy();
-  //   };
-  // }, []);
-
-  // TODO: Add more markers to the globe
-  //       Center the globe 
 
   return (
     <div id='wherewework' className='max-w-[90rem] w-full mx-auto flex flex-1 items-center justify-center' style={{ height: '100vh' }}>
@@ -54,11 +15,8 @@ const WhereWeWork = () => {
           </p>
         </div>
 
-        <div className='max-w-4xl px-6 md:px-8 pt-4 z-10 min-w-0 w-full flex justify-between'>
-          {/* <canvas
-            ref={canvasRef}
-            style={{ width: 600, height: 600, maxWidth: '100%', aspectRatio: 1 }}
-          /> */}
+        <div className='mx-auto max-w-2xl  w-full flex items-center justify-center'>
+          <MapChart/>
         </div>
       </div>
     </div>
